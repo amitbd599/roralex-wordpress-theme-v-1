72c662aeb4eaf40379d39032e09d8b7ded5c0c2d
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-05-22 21:30:44 +0600

    footer widget unit test pass

--- wp-content/themes/roralex/template-parts/footer/footer-1.php
+++ wp-content/themes/roralex/template-parts/footer/footer-1.php
@@ -47,8 +47,8 @@ case '3':
     $footer_class[3] = 'col-xl-4 col-lg-6';
     break;
 case '4':
-    $footer_class[1] = 'col-lg-4 col-md-6';
-    $footer_class[2] = 'col-lg-2 col-md-6';
+    $footer_class[1] = 'col-lg-3 col-md-6';
+    $footer_class[2] = 'col-lg-3 col-md-6';
     $footer_class[3] = 'col-lg-3 col-md-6';
     $footer_class[4] = 'col-lg-3 col-md-6';
     break;
@@ -71,11 +71,11 @@ default:
 
                 <?php
                     if ( $footer_columns < 4 ) {
-                    print '<div class="col-lg-4 col-md-6">';
+                    print '<div class="col-lg-3 col-md-6">';
                     dynamic_sidebar( 'footer-1' );
                     print '</div>';
 
-                    print '<div class="col-lg-2 col-md-6">';
+                    print '<div class="col-lg-3 col-md-6">';
                     dynamic_sidebar( 'footer-2' );
                     print '</div>';
 
