da074a919c11fae7f56ba864613537131e56c46f
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-05-27 01:25:16 +0600

    add update

--- wp-content/themes/roralex/archive.php
+++ wp-content/themes/roralex/archive.php
@@ -13,51 +13,61 @@ $blog_column = is_active_sidebar( 'blog-sidebar' ) ? 8 : 12;
 
 ?>
 
-<div class="tp-blog-area pt-120 pb-90">
-    <div class="container container-box">
+<section class="blog-group-version-one blog-details-version-one bg-white pt-80 pb-140">
+    <div class="container ">
         <div class="row">
-			<div class="col-lg-<?php print esc_attr( $blog_column );?> blog-post-items">
-				<div class="postbox__wrapper pr-20">
-				<?php if ( have_posts() ): ?>
-					<header class="page-header d-none">
-						<?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
-							the_archive_description( '<div class="archive-description">', '</div>' );
-						?>
-					</header><!-- .page-header -->
-					<?php
+            <div class="col-lg-<?php print esc_attr( $blog_column );?> blog-post-items blog-padding">
+                <div class="postbox__wrapper">
+                    <div class="left-side">
+                        <?php
+						if ( have_posts() ):
+    					if ( is_home() && !is_front_page() ):
+    				?>
+                        <header>
+                            <h1 class="page-title screen-reader-text"><?php single_post_title();?></h1>
+                        </header>
+                        <?php
+						endif;?>
+                        <?php
 						/* Start the Loop */
-						while ( have_posts() ):
-							the_post();
-
+						while ( have_posts() ): the_post(); ?>
+                        <?php
 							/*
 							* Include the Post-Type-specific template for the content.
 							* If you want to override this in a child theme, then include a file
 							* called content-___.php (where ___ is the Post Type name) and that will be used instead.
 							*/
-							get_template_part( 'template-parts/content', get_post_type() );
-						endwhile;
-					?>
+							get_template_part( 'template-parts/content', get_post_format() );?>
+                        <?php
+							endwhile;
+						?>
 
-					<div class="basic-pagination basic-pagination-2 mb-40">
-						<?php roralex_pagination( '<i class="fas fa-angle-double-left"></i>', '<i class="fas fa-angle-double-right"></i>', '', [ 'class' => '' ] );?>
-					</div>
-					<?php
+
+
+
+
+                        <div class="basic-pagination mb-40 pagination ">
+                            <?php roralex_pagination( '<i class="fa-light fa-angles-left"></i>', '<i class="fa-light fa-angles-right"></i>', '', ['class' => ''] );?>
+                        </div>
+                        <?php
 						else:
 							get_template_part( 'template-parts/content', 'none' );
 						endif;
 					?>
-				</div>
-			</div>
-			<?php if ( is_active_sidebar( 'blog-sidebar' ) ): ?>
-		        <div class="col-lg-4">
-		        	<div class="blog__sidebar pl-70">
-						<?php get_sidebar();?>
-	            	</div>
-	            </div>
-			<?php endif;?>
+
+                    </div>
+                </div>
+            </div>
+
+            <?php if ( is_active_sidebar( 'blog-sidebar' ) ): ?>
+            <div class="col-lg-4">
+                <div class="blog__sidebar">
+                    <?php get_sidebar();?>
+                </div>
+            </div>
+            <?php endif;?>
         </div>
     </div>
-</div>
+</section>
 <?php
 get_footer();
